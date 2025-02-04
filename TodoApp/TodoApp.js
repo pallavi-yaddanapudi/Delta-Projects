56288812b1117826ac7c22cbae30bc372b512e89
pallavi-yaddanapudi <yaddanapudipallavi101@gmail.com>
2025-02-04 15:49:12 +0530

    Added TODOAPP

---
+++ TodoApp/TodoApp.js
@@ -0,0 +1,30 @@
+let btn=document.querySelector("button");
+let input=document.querySelector("input");
+let ul=document.querySelector("ul");
+btn.addEventListener("click",function() {
+    let item=document.createElement("li");
+    item.innerText=input.value;
+
+    let delBtn=document.createElement("button");
+    delBtn.innerText="delete";
+    delBtn.classList.add("delete");
+
+    item.appendChild(delBtn);
+    ul.appendChild(item);
+    input.value="";
+});
+//Event Delegation
+ul.addEventListener("click",function(event) {
+    if(event.target.nodeName="BUTTON") {
+        let listItem=event.target.parentElement;
+        listItem.remove();
+    }
+});
+//  let delBtns=document.querySelectorAll(".delete");
+//  for(delBtn of delBtns)
+//  {
+//     delBtn.addEventListener("click",function() {
+//         let par=this.parentElement;
+//         par.remove();
+//     });
+//  }
